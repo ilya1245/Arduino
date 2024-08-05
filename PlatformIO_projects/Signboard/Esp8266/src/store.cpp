66d59c090265b0e3dc242bb28355bc9852702dd9
ilya1245 <ilya1245@gmail.com>
2024-08-06 00:47:51 +0300

    Esp8266 - Signboard - store last state V2

--- PlatformIO_projects/Signboard/Esp8266/src/store.cpp
+++ PlatformIO_projects/Signboard/Esp8266/src/store.cpp
@@ -1,25 +1,46 @@
 #include "store.h"
 #include "common.h"
+#include "tune.h"
 
 int isOnAddress = 0;
 int ledModeIrCommandAddress = 2;
+int timeIntervalAddress = 4;
+int pwmHighValueAddress = 8;
+int pwmLowValueAddress = 12;
+
+
 
 
 void writeSettings() {
   // if (isLedMode) {}
   EEPROM.write(isOnAddress, byte(isOn));
   printf("\nwrite isOn = %d", byte(isOn));  
-  printf("\nledModeIrCommand = %d", ledModeIrCommand);
+  printf("\nwrite ledModeIrCommand = %d", ledModeIrCommand);
   if (ledModeIrCommand > 0) {
     EEPROM.write(ledModeIrCommandAddress, byte(ledModeIrCommand));
     printf("\nwrite ledModeIrCommand = %d", ledModeIrCommand);
   } 
+  EEPROM.put(timeIntervalAddress, timeInterval);
+  printf("\nwrite timeInterval = %d", timeInterval);
+  EEPROM.put(pwmHighValueAddress, pwmHighValue);
+  printf("\nwrite pwmHighValue = %d", pwmHighValue);
+  EEPROM.put(pwmLowValueAddress, pwmLowValue);
+  printf("\nwrite pwmLowValue = %d", pwmLowValue);
+  printf("\n");
+
   EEPROM.commit();
 }
 
 void readSettings() {
   isOn = boolean(EEPROM.read(isOnAddress));
-  ledModeIrCommand = EEPROM.read(ledModeIrCommandAddress);
   printf("\nread isOn = %d", isOn);
+  ledModeIrCommand = EEPROM.read(ledModeIrCommandAddress);  
   printf("\nread ledModeIrCommand = %d", ledModeIrCommand);
+  EEPROM.get(timeIntervalAddress, timeInterval);
+  printf("\nread timeInterval = %d", timeInterval);
+  EEPROM.get(pwmHighValueAddress, pwmHighValue);
+  printf("\nread pwmHighValue = %d", pwmHighValue);
+  EEPROM.get(pwmLowValueAddress, pwmLowValue);
+  printf("\nread pwmLowValue = %d", pwmLowValue);
+  printf("\n");
 }
